76ec980640b3a975ee3314249fd5d83b5a6e0c41
unknown <rhett.gibbs@yahoo.com>
2024-03-07 14:58:54 -0800

    front end queries mutations reducers

---
+++ client/src/utils/actions.js
@@ -0,0 +1,11 @@
+// PRODUCT
+export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
+
+// CART
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const ADD_MULTIPLE_TO_CART = 'ADD_MULTIPLE_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const CLEAR_CART = 'CLEAR_CART';
+export const UPDATE_CART_QUANTITY = 'UPDATE_CART_QUANTITY';
+export const TOGGLE_CART = 'TOGGLE_CART';
+
