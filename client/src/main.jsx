e2565d05d729ad743af841ae542693def5ecc25b
Berie <bberaki2015@gmail.com>
2024-03-08 13:47:01 -0800

    created components and pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,11 @@ import './index.css'
 import App from './App.jsx'
 // import Error from './pages/Error';
 import Home from './pages/Home';
+import Detail from './pages/Detail';
+import NoMatch from './pages/NoMatch';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import OrderHistory from './pages/OrderHistory'
 
 // Define the router object which will control the Provider's ability to display certain pages to match the proper URLs
 const router = createBrowserRouter([
@@ -13,17 +18,25 @@ const router = createBrowserRouter([
     path: '/',
     element: <App />,
     // put error page
-    // errorElement: <Error />,
+    errorElement: <NoMatch />,
     children: [
       {
         index: true,
-        element: <Home />,
-      },
-      // {
-      //   path: '/thoughts/:thoughtId',
-      //   element: <SingleThought />,
-      // },
-    ],
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
+      }, {
+        path: '/orderHistory',
+        element: <OrderHistory />
+      }, {
+        path: '/products/:id',
+        element: <Detail />
+      }
+    ]
   },
 ]);
 
