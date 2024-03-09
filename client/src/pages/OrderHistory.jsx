d1c09495bac434c48a2dc298c13d1641a1a21a9f
unknown <rhett.gibbs@yahoo.com>
2024-03-09 14:31:12 -0800

    CSS fixes

---
+++ client/src/pages/OrderHistory.jsx
@@ -0,0 +1,14 @@
+import Cart from '../components/Cart';
+
+const OrderHistory = () => {
+    return (
+        <div>
+            <h1>Order History</h1>
+            <Cart>
+
+            </Cart>
+        </div>
+    )
+}
+  
+export default OrderHistory;
