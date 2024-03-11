e229842a6439173c87bc73fbb0d57a20599d4ef8
Berie <bberaki2015@gmail.com>
2024-03-10 23:11:06 -0700

    fixed the issue with signup and the cart

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,7 +13,7 @@ export const LOGIN = gql`
 
 export const ADD_USER = gql`
     mutation addUser($firstName: String!, $lastName: String!, $userName: String!, $password: String!, $email: String!) {
-        addUser(firstName: $firstName, lastName: $laastName, userName: $userName, password: $password, email: $email) {
+        addUser(firstName: $firstName, lastName: $lastName, userName: $userName, password: $password, email: $email) {
             token
             user {
                 _id
